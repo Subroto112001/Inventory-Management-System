a84a4e0c0a19497d6413740cae58bf7c79089bdc
Subroto112001 <subrotokumarbarman@gmail.com>
2026-06-02 19:16:06 +0600

    database name work perfectly

--- src/lib/databse/mongodb.js
+++ src/lib/databse/mongodb.js
@@ -1,6 +1,7 @@
 // lib/mongodb.js
 import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_DB = process.env.MONGODB_DB || "ims";
 
 const connectMongoDB = async () => {
   try {
@@ -8,7 +9,9 @@ const connectMongoDB = async () => {
     if (mongoose.connection.readyState === 1) {
       return mongoose.connection.asPromise();
     }
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(MONGODB_URI, {
+      dbName: MONGODB_DB,
+    });
     console.log("Connected to MongoDB successfully.");
   } catch (error) {
     console.error("Error connecting to MongoDB: ", error);
