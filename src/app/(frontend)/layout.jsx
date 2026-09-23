609c00ef45709cacd1ceff810083e82e0554b02a
Subroto112001 <subrotokumarbarman@gmail.com>
2026-09-23 20:50:17 +0600

    league spartan front is added from google by next/font/google

--- src/app/(frontend)/layout.jsx
+++ src/app/(frontend)/layout.jsx
@@ -1,15 +1,21 @@
 import FrontFooter from "@/Component/website/GlobalComponent/FrontFooter";
 import FrontHeader from "@/Component/website/GlobalComponent/FrontHeader";
+import { League_Spartan } from "next/font/google";
 
+const leagueSpartan = League_Spartan({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
+});
 
 export default function FrontendLayout({ children }) {
   return (
-    <>
+    <div className={leagueSpartan.className}>
       <FrontHeader />
 
       <main>{children}</main>
 
       <FrontFooter />
-    </>
+    </div>
   );
 }
